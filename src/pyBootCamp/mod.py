ea0c589f2adeef6087003eb4e3516c07c7de7ef3
Shubhojit Dasgupta <shubhojit.dasgupta@gmail.com>
2022-09-18 19:58:51 +0530

    Added modules with comments

---
+++ src/pyBootCamp/mod.py
@@ -0,0 +1,11 @@
+import time
+'''
+External module used as web development Framework. 
+Installed separately and imported
+in local modules for using its resources.
+'''
+#import flask
+
+# Waiting for 2 seconds
+time.sleep(2)
+print("2 seconds have passed since this program ran")
